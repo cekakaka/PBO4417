65521138342077794bed2c94c2a1179a88b57b66
dwicandrakirana <dwicandra920@gmail.com>
2021-03-05 14:13:32 +0700

    update java

--- Pertemuan1/Java/src/main/java/testSimpleCircle.java
+++ Pertemuan1/Java/src/main/java/testSimpleCircle.java
@@ -49,23 +49,29 @@ public class testSimpleCircle {
         myCircle1.getArea();
         System.out.println("Area Of The Circle 1 of Radius "+ myCircle1.radius + " is = "+ myCircle1.getArea());
         
-        // Set Radius 25
+        // Set Radius 9
         SimpleCircle myCircle2 = new SimpleCircle();
-        myCircle2.setRadius(25);
+        myCircle2.setRadius(9);
         myCircle2.getArea();
         System.out.println("Area Of The Circle 2 of Radius "+ myCircle2.radius + " is = "+ myCircle2.getArea());
         
-        // Set Radius 125
+        // Set Radius 28
         SimpleCircle myCircle3 = new SimpleCircle();
-        myCircle3.setRadius(125);
+        myCircle3.setRadius(28);
         myCircle3.getArea();
         System.out.println("Area Of The Circle 3 of Radius "+ myCircle3.radius + " is = "+ myCircle3.getArea());
         
-        //Modify Circle Radius
-        myCircle1.radius = 100;
+        //Modify Circle Radius 
+        myCircle1.radius = 57;
         myCircle1.getArea();
         System.out.println("Area Of The Circle 1 of Radius "+ myCircle1.radius + " is = "+ myCircle1.getArea());
         
+        
+        // Set Radius 18 n getParimeter
+        SimpleCircle myCircle4 = new SimpleCircle();
+        myCircle4.setRadius(18);
+        myCircle4.getPerimeter();
+        System.out.println("Perimeter of The Circle 4 of Radius "+ myCircle4.radius + "is = "+myCircle4.getPerimeter());
         
         
         
